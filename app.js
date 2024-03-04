486c1f2c275ccfcdaaf5a9129f7930903a2881c7
AbrarAhmed1990 <appdev1990w@gmail.com>
2024-03-04 00:46:08 -0800

    added Answer of Chapter 14-16

---
+++ app.js
@@ -0,0 +1,29 @@
+    //   <------ CHAPTER N0 14------------->
+// Q no 1------------->
+// var arr1;
+
+// Q no 2------------->
+// var arr=[];
+
+// Q no 3------------->
+// var arr2=["Husnain","Zain","Saif"];
+
+// Q no 4------------->
+// var arr3=[1,2,3,4,5];
+
+// Q no 5------------->
+// var arr4=[true,false.ture,true];
+
+// Q no 6------------->
+// var arr5=["Husnain",15,true];
+
+// Q no 7------------->
+
+// var eduQ = ["SSC", "HSC", "BCS", "BS", "BS", "BCOM", "MS", "M. Phil.", "PhD"];
+
+// document.write("<h1>Qualifications:</h1>");
+// document.write("<ol>");
+// for (var i = 0; i < eduQ.length; i++) {
+//   document.write("<li>" + eduQ[i] + "</li>");
+// }
+// document.write("</ol>");
